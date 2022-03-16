ee6a9daa71fea115467d80832af6c2d170893922
Guilherme Maiuri <guilhermemgatti@gmail.com>
2022-03-16 00:30:57 -0300

    Validação de campo cnpj

--- Pratica/scripts/index.js
+++ Pratica/scripts/index.js
@@ -1,7 +1,37 @@
+const INVALID_FORMAT = "Formato inválido"
+
 $(document).ready(function() {
-  debugger
-  var emailError = $(email_erro)
-  emailError.addClass('correct')
-  emailError.text("teste 123")
+  
+  $(cod_cnpj).keyup(validateCNPJFormat)
+  $(cod_cnpj).focus(validateCNPJFormat)
 })
 
+function validateCNPJFormat(event) {
+  const cnpjErro = $(cnpj_erro)
+  const cnpjValor = $(cod_cnpj)
+  if(!formatarCNPJ(cnpjValor[0])) {
+    cnpjErro.removeClass('hidden')
+    cnpjErro.text(INVALID_FORMAT)
+    turnInvalid(cnpjValor)
+  } else {
+    cnpjErro.addClass('hidden')
+    turnValid(cnpjValor)
+  }
+
+}
+
+function formatarCNPJ(value) {
+  
+  const cnpjRegex =/\w{2}\.\w{3}\.\w{3}\/\w{4}\-\w{2}/
+  return cnpjRegex.test(value.value)
+}
+
+function turnValid(value) {
+  value.addClass("valid")
+  value.removeClass("invalid")
+}
+
+function turnInvalid(value) {
+  value.addClass("invalid")
+  value.removeClass("valid")
+}
